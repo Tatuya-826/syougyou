35fa40ac3a53fe7ecea3c205d4410c3ea0bfd94f
nomuratouwa <81787326+nomuratouwa@users.noreply.github.com>
2021-09-27 09:39:45 +0900

    komitto

--- Assets/Scripts/Game/Object/Enemy/ShooterPatern.cs
+++ Assets/Scripts/Game/Object/Enemy/ShooterPatern.cs
@@ -2,45 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimplePatarn : MonoBehaviour
+public class ShooterPatern : MonoBehaviour
 {
     public GameObject Effect;
     public int AttackType;
-    public const float AttackDelay = 1.0f;
-    public float DelayTime;
 
     PlayerGoScript moveScript;          //移動スクリプト
     public bool attackFrag = false;
     public bool Tuibi = false;
+    public GameObject bulletObject;
+    public float bulletSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         moveScript = this.gameObject.GetComponent<PlayerGoScript>();
-        DelayTime = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         switch (attackFrag) {
             case false:
                 if(Tuibi)
                     moveScript.PlayerTuibi();
                 break;
             case true:
-                if(DelayTime<0)
                 Attack();
                 break;
         }
-        if (DelayTime > 0)
-            DelayTime -= 0.1f;
     }
 
     void Attack()
     {
-        
-        Instantiate(Effect, this.transform.position, Quaternion.identity);
-        DelayTime = AttackDelay;
+        Instantiate(Effect, new Vector3(1.0f, 0.0f, 0.0f), Quaternion.identity);
+        var Bullet = Instantiate(bulletObject,this.transform.position, this.transform.rotation);
+
+        attackFrag = false;
     }
 }
