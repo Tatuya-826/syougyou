f9b9823c946931f1b656831c39bf0b7ebd4ff474
ty2uuu <san-a19039@sist.ac.jp>
2021-10-04 16:16:21 +0900

    1004作業 セーブ

---
+++ Assets/Scripts/Game/Object/Player/NetCameraScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//カメラが主人公に追従するためのプログラム
+public class NetCameraScript : MonoBehaviour
+{
+    [SerializeField] private GameObject player;   //プレイヤー情報格納用
+    [SerializeField] private Vector3 offset;    //カメラとプレイヤの距離
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+
+    }
+
+    void cameraSet()
+    {
+
+    }
+}
