c72df381d4c30e2ca905e302325184ff23733b58
ty2uuu <rin0916game@gmail.com>
2021-11-04 16:21:16 +0900

    no message

--- Assets/Scripts/Game/Object/Enemy/EnemyHitScript.cs
+++ Assets/Scripts/Game/Object/Enemy/EnemyHitScript.cs
@@ -46,7 +46,6 @@ public class EnemyHitScript : MonoBehaviourPunCallbacks, IPunObservable
     {
         if (HP <= 0)
         {
-
             print("デストロイ");
             Destroy(oyaObject);
         }
