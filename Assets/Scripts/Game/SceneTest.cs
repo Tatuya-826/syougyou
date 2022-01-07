a97343f3cd71e9aa75cf115d256da4cda654e6f4
aaa <aaa>
2022-01-07 12:32:40 +0900

    no message

---
+++ Assets/Scripts/Game/SceneTest.cs
@@ -0,0 +1,20 @@
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.IsMessageQueueRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
