c69b09f5488e0e9d6e1ca3c670c9758e65815146
ty2uuu <rin0916game@gmail.com>
2022-01-13 15:28:07 +0900

    no message

---
+++ Assets/Scripts/Enemy/enemyRespawn.cs
@@ -0,0 +1,36 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enemyRespawn : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("接続完了");
+        /////////////////////////////////////////////////////////////////////////
+        //リスポーンポイントの座標を取得し、その場所にアーサーくんを配位する
+        Transform myTransform = this.transform;
+
+        Vector3 pos = myTransform.position;
+        //座標の格納
+        var rPosition = new Vector3(pos.x,pos.y,pos.z);
+
+        //アーサーくん配置
+        PhotonNetwork.Instantiate("Kinkyori", rPosition, Quaternion.identity);
+
+        //cameraScript.player = GameObject.Find("NetArthur");
+    }
+}
