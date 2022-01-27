8c7015ca5c2cca759234f3ee26325d22dfbe3ac0
nomuratouwa <81787326+nomuratouwa@users.noreply.github.com>
2022-01-27 10:22:32 +0900

    static

--- Assets/Scripts/Player/PlayerNowWeapon.cs
+++ Assets/Scripts/Player/PlayerNowWeapon.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerNowWeapon : MonoBehaviour
 {
+    
+
     public readonly static PlayerNowWeapon PlayerEquipment = new PlayerNowWeapon();
 
     public int WeaponID=0;
